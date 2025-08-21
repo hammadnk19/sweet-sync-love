ea9ec34a4da428f0eaaf568e7d7053d1e6086d3e
hammadnk19 <hammadtoon@gmail.com>
2025-08-21 09:55:37 -0700

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -47,6 +47,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
+        window.location.replace("https://web.whatsapp.com/");
       />
     )
   }
